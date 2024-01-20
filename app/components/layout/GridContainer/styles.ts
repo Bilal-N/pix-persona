b22240158604e56e2b7bf3bebe17ef133b1028f9
Bilal <bilal@blinkco.io>
2024-01-20 19:06:22 +0500

    fix: added max-width directly

--- app/components/layout/GridContainer/styles.ts
+++ app/components/layout/GridContainer/styles.ts
@@ -2,7 +2,7 @@ import { css } from "../../../../styled-system/css";
 import { gridContainerWidth } from "../../../css/structure";
 
 export const gridContainer = css({
-  maxWidth: gridContainerWidth,
+  maxWidth: "1080px",
   marginLeft: "auto",
   marginRight: "auto",
 });
