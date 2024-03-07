abc7549be7f0cdf95d301c3b16ecd4f351cfd20e
Bilal <bilal@blinkco.io>
2024-03-07 21:52:19 +0500

    chore: added link on header logo

--- app/components/layout/Header/index.tsx
+++ app/components/layout/Header/index.tsx
@@ -1,6 +1,7 @@
 import { Grid, Cell } from "@faceless-ui/css-grid";
 import { header } from "./styles";
 import { LogoWithText } from "../../graphics/Logo";
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -8,7 +9,9 @@ const Header = () => {
       <div className={header}>
         <Grid>
           <Cell cols={12}>
-            <LogoWithText />
+            <Link href="/">
+              <LogoWithText />
+            </Link>
           </Cell>
         </Grid>
       </div>
