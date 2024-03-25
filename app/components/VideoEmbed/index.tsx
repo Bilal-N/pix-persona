caf3dda8e233e909dbc3ca89aac7fba593a6f955
Bilal <bilal@blinkco.io>
2024-03-25 16:26:18 +0500

    fix: removed frameborder attribute

--- app/components/VideoEmbed/index.tsx
+++ app/components/VideoEmbed/index.tsx
@@ -7,7 +7,6 @@ const VideoEmbed = ({ source }) => {
         <span></span>
         <iframe
           src={source}
-          frameborder="0"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
           title="showreel2"
         ></iframe>
